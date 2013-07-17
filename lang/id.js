801429487775e220623e50185e89da6ecf0fe1cf
Wiktor Walc <w.walc@cksource.com>
2013-07-17 17:38:41 +0200

    Updated translations, added Indonesian language.

--- lang/id.js
+++ lang/id.js
@@ -2,8 +2,8 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'smiley', 'sl', {
-	options: 'Možnosti Smeška',
-	title: 'Vstavi smeška',
-	toolbar: 'Smeško'
+CKEDITOR.plugins.setLang( 'smiley', 'id', {
+	options: 'Opsi Smiley',
+	title: 'Sisip sebuah Smiley',
+	toolbar: 'Smiley'
 });
